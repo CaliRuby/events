63174e6fc19e8544dd0c64b36ce17c10627d2aaf
Moises Vargas M <moisesweb@gmail.com>
2015-04-25 12:37:33 -0400

    envet user relation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150425160034) do
+ActiveRecord::Schema.define(version: 20150425163240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20150425160034) do
     t.string   "name"
     t.string   "place"
     t.datetime "start_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "events_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
